628735628b3bc44319a0a62a8a51162f53f7be09
Elias Büchel <eli.buechel@hotmail.com>
2023-07-04 17:13:59 +0200

    Setting up Solution

---
+++ source/BusinessLayer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BusinessLayer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ source/DataLayer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DataLayer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
