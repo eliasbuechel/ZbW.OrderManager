edbf452dbaad597dfea5c0fa9595d89bc860af39
Shannon Würzer <shannon.wuerzer@bluewin.ch>
2023-09-17 19:30:09 +0200

    add regex for validation

--- source/src/DataLayer/Customers/Validation/CustomerValidator.cs
+++ source/src/DataLayer/Customers/Validation/CustomerValidator.cs
@@ -80,15 +80,15 @@ namespace DataLayer.Customers.Validation
         }
 
         private const string CUSTOMER_NR_VALIDATION_PATTERN = @"^CU\d{5}$";
-        private const string FIRST_NAME_VALIDATION_PATTERN = @"^.+$";
-        private const string LAST_NAME_VALIDATION_PATTERN = @"^.+$";
-        private const string STREET_NAME_VALIDATION_PATTERN = @"^.+$";
-        private const string HOUSE_NUMBER_VALIDATION_PATTERN = @"^.+$";
-        private const string POSTAL_CODE_VALIDATION_PATTERN = @"^.+$";
-        private const string CITY_VALIDATION_PATTERN = @"^.+$";
-        private const string EMAIL_ADDRESS_VALIDATION_PATTERN = @"^.+$";
-        private const string WEBSITE_URL_VALIDATION_PATTERN = @"^.+$";
-        private const string PASSWORD_VALIDATION_PATTERN = @"^.+$";
+        private const string FIRST_NAME_VALIDATION_PATTERN = @"^(.{1,100})$";
+        private const string LAST_NAME_VALIDATION_PATTERN = @"^(.{1,100})$";
+        private const string STREET_NAME_VALIDATION_PATTERN = @"^(.{1,200})$";
+        private const string HOUSE_NUMBER_VALIDATION_PATTERN = @"^(.{1,10})$";
+        private const string POSTAL_CODE_VALIDATION_PATTERN = @"^(.{1,10})$";
+        private const string CITY_VALIDATION_PATTERN = @"^(.{1,100})$";
+        private const string EMAIL_ADDRESS_VALIDATION_PATTERN = @"^(?=[\S\s]{1,200}$)[a-zA-Z0-9!#$&_*?^{}-]+(\.[a-zA-Z0-9!#$&_*?^{}-]+)*@([a-zA-Z0-9]+([a-zA-Z0-9-]*)\.)+[a-zA-Z]+";
+        private const string WEBSITE_URL_VALIDATION_PATTERN = @"^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,253}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*){4,255}$";
+        private const string PASSWORD_VALIDATION_PATTERN = @"(?=.*[0-9])(?=.*[a-zäöü])(?=.*[A-Z])[a-zäöüA-ZÄÖÜ0-9!#$&_*?^{}-]{8,255}";
         private const string HASHED_PASSWORD_VALIDATION_PATTERN = @"^.+$";
     }
 }
